a906067887a6871075bfd92f85438fe91a27f476
wbvtler <wesleybutler.code@gmail.com>
2020-06-15 15:52:47 -0500

    Add 'delete logs' to state via redux

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,26 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
+import { deleteLog } from '../../actions/logActions';
 
-const LogItem = ({ log }) => {
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const LogItem = ({ log, deleteLog }) => {
+  const { id, tech, attention, date, message } = log;
+
+  const onDelete = () => {
+    deleteLog(log.id);
+    M.toast({ html: 'Log Deleted' });
+  };
+
   return (
     <li className="collection-item">
       <div>
         <a
           href="#edit-log-modal"
-          className={`modal-trigger ${
-            log.attention ? 'red-text' : 'blue-text'
-          }`}
+          className={`modal-trigger ${attention ? 'red-text' : 'blue-text'}`}
         >
-          {log.message}
+          {message}
         </a>
         <br />
         <span className="grey-text">
-          <span className="black-text">ID #{log.id}</span> last updated by{' '}
-          <span className="black-text">{log.tech}</span> on{' '}
-          <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
+          <span className="black-text">ID #{id}</span> last updated by{' '}
+          <span className="black-text">{tech}</span> on{' '}
+          <Moment format="MMMM Do YYYY, h:mm:ss a">{date}</Moment>
         </span>
-        <a href="#!" className="secondary-content">
+        <a href="#!" onClick={onDelete} className="secondary-content">
           <i className="material-icons grey-text">delete</i>
         </a>
       </div>
@@ -29,7 +38,8 @@ const LogItem = ({ log }) => {
 };
 
 LogItem.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired
 };
 
-export default LogItem;
+export default connect(null, { deleteLog })(LogItem);
